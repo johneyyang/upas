3d760fdf2b35acb0d8a7cbb71fa9bcc77a312187
nickgood <n.good@colostate.edu>
2017-03-26 18:17:18 -0600

    start app template

---
+++ app.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(leaflet)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+ fluidRow(
+  column(2, p(), "UPAS test app", p()
+  ),
+  column(2, offset = 2, p(), "load button here", p()
+  )      
+ ),
+  fluidRow(
+    leafletOutput("mymap"),
+    p(),
+    actionButton("recalc", "New points")
+  )
+)
+
+server <- function(input, output, session){
+
+  points <- eventReactive(input$recalc, {
+  cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+}, ignoreNULL = FALSE)
+
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+    addProviderTiles(providers$Stamen.TonerLite,
+                     options = providerTileOptions(noWrap = TRUE)
+  ) %>%
+    addMarkers(data = points())
+  })
+}
+
+shinyApp(ui, server)
